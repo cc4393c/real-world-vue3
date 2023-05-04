e12f356ef95d01111ef7a8477005c989d30e0e7b
chen4393c <chen4393c@163.com>
2023-05-04 10:36:49 +0800

    building pagination

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // singleton axios instance
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/cc4393c/events',
+  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Touring-Vue-Router',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
@@ -11,8 +11,8 @@ const apiClient = axios.create({
 })
 
 export default {
-  getEvents() {
-    return apiClient.get('/events')
+  getEvents(perPage, page) {
+    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
   },
   getEvent(id) {
     return apiClient.get('/events/' + id)
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,18 +1,30 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { computed, ref } from 'vue'
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from 'vue'
+
+const props = defineProps(['page'])
 
 const events = ref(null)
+const totalEvents = ref(0)
 
-onMounted(async () => {
+watchEffect(async () => {
+  events.value = null
+  totalEvents.value = 0
   // get events from mock db when component is created
   try {
-    const response = await EventService.getEvents()
+    const response = await EventService.getEvents(2, props.page)
     events.value = response.data
+    totalEvents.value = response.headers['x-total-count']
   } catch (error) {
     console.log(error)
   }
+})
+
+const hasNextPage = computed(() => {
+  const totalPages = Math.ceil(totalEvents.value / 2)
+  return props.page < totalPages
 })
 </script>
 
@@ -21,6 +33,25 @@ onMounted(async () => {
     <h1>Events For Good</h1>
     <div class="events">
       <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+      <div class="pagination">
+        <RouterLink
+          id="page-prev"
+          :to="{ name: 'event-list', query: { page: page - 1 } }"
+          rel="prev"
+          v-if="page != 1"
+        >
+          &#60; Prev
+        </RouterLink>
+
+        <RouterLink
+          id="page-next"
+          :to="{ name: 'event-list', query: { page: page + 1 } }"
+          rel="next"
+          v-if="hasNextPage">
+          Next &#62;
+        </RouterLink>
+      </div>
     </div>
   </div>
 </template>
@@ -29,6 +60,25 @@ onMounted(async () => {
 .events {
   display: flex;
   flex-direction: column;
-  align-items: center; /* center horizontally */
+  align-items: center;
+  /* center horizontally */
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
